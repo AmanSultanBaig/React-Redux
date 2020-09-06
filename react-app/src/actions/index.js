2ec4d64b677b73b53a5e21b9324e057ee1d56cb8
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-09-06 14:36:57 +0500

    user reducers actions defined as well

--- react-app/src/actions/index.js
+++ react-app/src/actions/index.js
@@ -3,13 +3,29 @@ export const increment = () => {
         type: 'INCREMENT'
     }
 }
+
 export const decrement = () => {
     return {
         type: 'DECREMENT'
     }
 }
+
 export const reset = () => {
     return {
         type: 'RESET'
+    }
+}
+
+export const add_user = data => {
+    return {
+        type: 'ADD_USER',
+        payload: data
+    }
+}
+
+export const remove_user = index => {
+    return {
+        type: 'REMOVE_USER',
+        payload: index
     }
 }
